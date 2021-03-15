30c47a1bd832bd99fb8e9a1ceaf05c4a8364c8b3
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-15 02:05:47 -0400

    Add useReactiveVar for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { useReactiveVar } from '@apollo/client';
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './screens/Home';
-import Home from './screens/Login';
+import { isLoggedInVar } from './apollo';
+import Home from './screens/Home';
+import Login from './screens/Login';
 import NotFound from './screens/NotFound';
 
 const App: React.FC = () => {
-  const isLoggedIn = true;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <div>
       <Router>
         <Switch>
           <Route path='/' exact>
-            { isLoggedIn ? <Home/> : <Login/> }
+            { isLoggedIn ? (
+              <Home />
+            ) : (
+              <Login />
+            ) }
           </Route>
           <Route>
             <NotFound/>
