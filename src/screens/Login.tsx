5109048e481244c4c08fc580516a82ad54edb34e
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-15 20:47:29 -0400

    Add Login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,19 +1,138 @@
 import styled from "styled-components";
-import { darkModeVar, isLoggedInVar } from "../apollo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const Container = styled.div``
-// background-color: ${(props) => props.theme.bgColor }
-const Title = styled.h1`
-    color: ${(props) => props.theme.fontColor};
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-`
+const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+    max-width: 350px;
+    width: 100%;
+`;
+
+const WhiteBox = styled.div`
+    background-color: white;
+    border: 1px solid ${(props) => props.theme.borderColor};
+    width: 100%;
+`;
+
+const Seperator = styled.div`
+    margin: 20px 0;
+    text-transform: uppercase;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    align-items: center;
+    div {
+        width: 100%;
+        height: 1px;
+        background-color: rgb(219, 219, 219);
+    }
+    span {
+        margin: 0 10px;
+        color: rgb(219, 219, 219);
+        font-weight: 600;
+    }
+`;
+
+const TopBox = styled(WhiteBox)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 35px 40px 10px 40px;
+    margin-bottom: 10px;
+    form {
+        margin-top: 35px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+`;
+
+const Input = styled.input`
+    width: 100%;
+    padding: 7px;
+    background-color: #FAFAFA;
+    border: 0.5px solid ${(props) => props.theme.borderColor};
+    border-radius: 3px;
+    margin-top: 5px;
+    box-sizing: border-box;
+    &::placeholder {
+        font-size: 13px;
+    }
+`;
+
+const Button = styled.input`
+    width: 100%;
+    border: none;
+    margin-top: 15px;
+    background-color: ${(props) => props.theme.accent};
+    color: white;
+    text-align: center;
+    padding: 6px 0px;
+    font-weight: 600;
+`;
+
+const FacebookLogin = styled.div`
+    margin: 10px 0 20px;
+    color: #385285;
+    span {
+        margin-left: 10px;
+        font-weight: 600;
+    }
+`;
+
+const BottomBox = styled(WhiteBox)`
+    padding: 20px 0px;
+    text-align: center;
+    a {
+        margin-left: 5px;
+        font-weight: 600;
+        color: rgb(0, 149, 246);
+    }
+`;
+
 
 const Login: React.FC = () => {
     return (
         <Container>
-            <Title>Login</Title>
-            <button onClick={() => darkModeVar(true)}>To dark</button>
-            <button onClick={() => darkModeVar(false)}>To light</button>
+            <Wrapper>
+                <TopBox>
+                    <div>
+                        <FontAwesomeIcon icon={faInstagram} size="3x" />
+                    </div>
+                    <h1>PGallery</h1>
+                    <form>
+                        <Input type="text" placeholder="Username" />
+                        <Input type="password" placeholder="Password" />
+                        <Button type="submit" value="Log in"/>
+                    </form>
+                    <Seperator>
+                        <div/>
+                        <span>Or</span>
+                        <div/>
+                    </Seperator>
+                    <FacebookLogin>
+                        <FontAwesomeIcon icon={faFacebookSquare}/>
+                        <span>
+                            Log in with Facebook
+                        </span>
+                    </FacebookLogin>
+                </TopBox>
+                <BottomBox>
+                    <span>Don't have an account?</span>
+                    <a href="#">Sing up</a>
+                </BottomBox>
+            </Wrapper>
+        
         </Container>
     )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -4,21 +4,39 @@ import reset from 'styled-reset';
 export const myTheme: DefaultTheme = {
     bgColor: "black",
     fontColor: "white",
+    accent: "rgb(0, 149, 246)",
+    borderColor: "rgb(219, 219, 219)",
 }
 
 export const lightTheme = {
     fontColor: "#2c2c2c",
     bgColor: "lightgray",
-    }
+    accent: "rgb(0, 149, 246)",
+    borderColor: "rgb(219, 219, 219)"
+}
   
 export const darkTheme = {
     fontColor: "lightgray",
     bgColor: "#2c2c2c",
+    accent: "rgb(0, 149, 246)",
+    borderColor: "rgb(219, 219, 219)"
 }
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
+    input {
+        all: unset;
+    }
+    * {
+        box-sizing: border-box;
+    }
     body {
-        background-color: ${(props) => props.theme.bgColor };
+        background-color: #FAFAFA;
+        font-size: 14px;
+        font-family: 'Open Sans', sans-serif;
+        color: rgb(38, 38, 38);
+    }
+    a {
+        text-decoration: none;
     }
 `
