e61570edce196da0b1e7e8b0e19151e659a8f4af
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-19 23:35:49 -0400

    Add photoComponents on Home

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,27 +1,32 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-const StyledAvatar = styled.div`
-    width: 27px;
-    height: 27px;
-    border-radius: 15px;
-    background-color: #2c2c2c;
-    overflow: hidden;
+interface IStyledAvatar {
+  lg: boolean;
+}
+
+const StyledAvatar = styled.div<IStyledAvatar>`
+  width: ${(props) => (props.lg ? '35px' : '27px')};
+  height: ${(props) => (props.lg ? '35px' : '27px')};
+  border-radius: 50%;
+  background-color: #2c2c2c;
+  overflow: hidden;
 `;
 
 const Img = styled.img`
-    max-width: 100%;
+  max-width: 100%;
 `;
 
 interface IAvatar {
-    url: string
+  url: string;
+  lg: boolean;
 }
 
-const Avatar: React.FC<IAvatar> = ({ url }) => {
-    return (
-        <StyledAvatar>
-            { url !== "" ? <Img alt="me" src={url} /> : null }
-        </StyledAvatar>
-    )
-}
+const Avatar: React.FC<IAvatar> = ({ url, lg = false }) => {
+  return (
+    <StyledAvatar lg={lg}>
+      {url !== '' ? <Img alt="me" src={url} /> : null}
+    </StyledAvatar>
+  );
+};
 
 export default Avatar;
--- src/components/shared.tsx
+++ src/components/shared.tsx
@@ -1,16 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const LogoBase = styled.span`
+  font-family: 'Pacifico', cursive;
+`;
 
 export const BaseBox = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-    border: 1px solid ${(props) => props.theme.borderColor};
-    width: 100%;
+  background-color: ${(props) => props.theme.bgColor};
+  border: 1px solid ${(props) => props.theme.borderColor};
+  width: 100%;
 `;
 
 export const FatLink = styled.span`
-    font-weight: 600;
-    color: rgb(142, 142, 142);
+  font-weight: 600;
+  color: rgb(142, 142, 142);
 `;
 
-export const LogoBase = styled.span`
-    font-family: 'Pacifico', cursive;
+export const FatText = styled.span`
+  font-weight: 600;
+  color: rgb(32, 32, 32);
 `;
