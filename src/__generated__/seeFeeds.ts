410232e4c5ee906270dd5877bbbd011d99678efb
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-22 22:32:10 -0400

    Add CommentCount and Comments

--- src/__generated__/seeFeeds.ts
+++ src/__generated__/seeFeeds.ts
@@ -13,6 +13,21 @@ export interface seeFeeds_seeFeeds_user {
   avatar: string | null;
 }
 
+export interface seeFeeds_seeFeeds_comments_user {
+  __typename: "User";
+  username: string;
+  avatar: string | null;
+}
+
+export interface seeFeeds_seeFeeds_comments {
+  __typename: "Comment";
+  id: number;
+  user: seeFeeds_seeFeeds_comments_user;
+  payload: string;
+  isMine: boolean;
+  createdAt: string;
+}
+
 export interface seeFeeds_seeFeeds {
   __typename: "Photo";
   id: number;
@@ -20,7 +35,8 @@ export interface seeFeeds_seeFeeds {
   image: string;
   caption: string | null;
   likes: number;
-  comments: number;
+  commentNumber: number;
+  comments: (seeFeeds_seeFeeds_comments | null)[] | null;
   createdAt: string;
   isMine: boolean;
   isLiked: boolean;
