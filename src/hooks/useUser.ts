1206779282f6aba80795e6e1a283f6c38c1e32c2
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-23 22:42:39 -0400

    remove unused seeMe

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,7 +1,6 @@
 import { gql, useQuery, useReactiveVar } from '@apollo/client';
 import { useEffect } from 'react';
 import { isLoggedInVar, logUserOut } from '../apollo';
-import { seeMe } from '../__generated__/seeMe';
 
 const SEE_ME_QUERY = gql`
   query seeMe {
